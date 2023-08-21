c7add595f0ac945ac93c04a867116093f5a30d3a
davique0 <davique@gmail.com>
2023-08-21 12:25:00 -0700

    Built, Travel, layout

--- app/(home)/Travel.tsx
+++ app/(home)/Travel.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Card from '../(shared)/Card'
 
 type Props = {}
 
@@ -8,10 +9,29 @@ const Travel = (props: Props) => {
       <hr className='border-1' />
       {/* Header */}
       <div className='flex items-center gap-3 my-8'>
-        <h4 className='bg-accent-orange py-2 px-5 text-wh-900 text-sm'>HOT</h4>
-        <p className='font-bold text-2xl'>Latest News in Technology</p>
-
+        <h4 className='bg-accent-orange py-2 px-5 text-wh-900 text-sm'>TRAVEL</h4>
+        <p className='font-bold text-2xl'>New Travel Experiences</p>
       </div>
+      {/* Cards */}
+      <div className='sm:flex justify-between gap-8'>
+        <Card
+          className='basis-1/3 bg-wh-500 mt-5 sm:mt-0'
+          imageHeight='h-80'
+        />
+        <Card
+          className='basis-1/3 bg-wh-500 mt-5 sm:mt-0'
+          imageHeight='h-80'
+        />
+        <Card
+          className='basis-1/3 bg-wh-500 mt-5 sm:mt-0'
+          imageHeight='h-80'
+        />
+      </div>
+      <Card
+        className='bg-wh-500 sm:flex justify-between items-center gap-3 mt-7 mb-5'
+        imageHeight='h-80'
+      />
+
     </section>
   )
 }
