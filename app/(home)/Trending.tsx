4d554b2b22b4a2aa7ce03549b13614a1a7b39790
davique0 <davique@gmail.com>
2023-08-21 18:12:36 -0700

    Feature: grab data from prisma

--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -1,16 +1,17 @@
+import { Post } from '@prisma/client';
 import Link from 'next/link'
 import React from 'react'
 
 type TrendingCardsProps = {
   className?: string;
+  post: Post;
 }
 
-const TrendingCard = ({ className }: TrendingCardsProps) => {
+const TrendingCard = ({ className, post }: TrendingCardsProps) => {
   return (
     <Link
       className={`${className} sm:mt-0 sm:h-auto relative mt-7 block w-full h-96 hover:opacity-70`}
-      // href={`${process.env.Next_PUBLIC_BASE_URL}/post/${post?.id}`}
-      href="/"
+      href={`${process.env.Next_PUBLIC_BASE_URL}/post/${post?.id}`}
     >
       <div className='z-0 relative w-full h-full bg-wh-500'>
         image
@@ -18,18 +19,20 @@ const TrendingCard = ({ className }: TrendingCardsProps) => {
       <div className='absolute z-1 top-0 left-0 w-full h-full bg-gradient-gradual'></div>
       <div className='absolute z-2 bottom-0 left-0 p-3'>
         <h4 className='inline-block px-5 py-1 font-semibold bg-accent-orange text-wh-900'>
-          Category
+          {post?.category}
         </h4>
-        <div className='text-wh-100 mt-2'>post title</div>
+        <div className='text-wh-100 mt-2'>{post?.title}</div>
       </div>
 
     </Link>
   )
 }
 
-type Props = {}
+type Props = {
+  trendingPosts: Array<Post>;
+};
 
-const Trending = (props: Props) => {
+const Trending = ({ trendingPosts }: Props) => {
   return (
     <section className='pt-3 pb-10'>
       <div className='flex items-center gap-3'>
@@ -54,10 +57,10 @@ const Trending = (props: Props) => {
       </div> */}
       {/* Grid */}
       <div className='sm:grid gap-5 grid-cols-4 grid-rows-2 sm:h-[600px]'>
-        <TrendingCard className='col-span-2 row-span-2 bg-wh-500' />
-        <TrendingCard className='col-span-2 row-span-1 bg-wh-500' />
-        <TrendingCard className='col-span-1 row-span-1 bg-wh-500' />
-        <TrendingCard className='col-span-1 row-span-1 bg-wh-500' />
+        <TrendingCard className='col-span-2 row-span-2 bg-wh-500' post={trendingPosts[0]} />
+        <TrendingCard className='col-span-2 row-span-1 bg-wh-500' post={trendingPosts[1]} />
+        <TrendingCard className='col-span-1 row-span-1 bg-wh-500' post={trendingPosts[2]} />
+        <TrendingCard className='col-span-1 row-span-1 bg-wh-500' post={trendingPosts[3]} />
       </div>
       <p className='text-sm'>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga nisi laboriosam excepturi error provident itaque saepe cumque dolores qui omnis aliquam dignissimos, voluptatum sapiente corrupti at distinctio repellat ipsa totam.
